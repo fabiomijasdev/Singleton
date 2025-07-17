f6b85e2543429a4684f42b4a6244ac99444f2aa6
fabiomija <fabio.mijas@gmail.com>
2025-07-18 00:28:09 +0100

    Add project files.

---
+++ Singleton/ClassicSingletonLogger.cs
@@ -0,0 +1,28 @@
+﻿namespace Singleton;
+
+public class ClassicSingletonLogger
+{
+    private static ClassicSingletonLogger? instance;
+
+    private ClassicSingletonLogger() { }
+
+    public static ClassicSingletonLogger GetInstance()
+    {
+        if (instance is null)
+            instance = new ClassicSingletonLogger();
+
+        return instance;
+    }
+
+    public static void Log(Exception exception)
+    {
+        Console.WriteLine(exception.ToString());
+    }
+
+    public static void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+
+}
