2a26bd288161481d0aebf779e286f1aecfec2dda
fabiomija <fabio.mijas@gmail.com>
2025-07-18 02:43:51 +0100

    Added StaticSingletonLogger

---
+++ Singleton/StaticSingletonLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace Singleton;
+
+public class StaticSingletonLogger : SingletonBase
+{
+    private static readonly StaticSingletonLogger instance = new StaticSingletonLogger();
+    private StaticSingletonLogger() { }
+
+    public static StaticSingletonLogger GetInstance()
+    {
+        return instance;
+    }
+}
